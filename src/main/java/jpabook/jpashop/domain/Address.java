9d3a07c690f607625d6f7d3f4f6c0920708d79d3
이기정 <kee@kee.local>
2024-07-15 18:35:39 +0900

    add Entity class

--- src/main/java/jpabook/jpashop/domain/Address.java
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -8,8 +8,15 @@ import lombok.Getter;
 public class Address {
 
     private String city;
-
     private String street;
-
     private String zipcode;
+
+    protected Address() {
+    }
+
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
 }
